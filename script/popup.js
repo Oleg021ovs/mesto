06e574079925a3dfa1c90326f109fba85b3cd1e4
Oleg Samoilenko <olaggg-93so@yandex.ru>
2022-03-17 01:32:38 +0300

    q

--- script/popup.js
+++ script/popup.js
@@ -3,9 +3,9 @@ const openPopup = document.querySelector('.popup-open');
 const closePopup = popup.querySelector('.popup__close');
 
 openPopup.addEventListener('click', function() {
-    popup.classList.add('popup__open')
+    popup.classList.add('popup__open_opened')
 });
 
 closePopup.addEventListener('click', function() {
-    popup.classList.remove('popup__open')
+    popup.classList.remove('popup__open_opened')
 });
