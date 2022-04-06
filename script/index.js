8e514a55c7539ee07d5b4f209e0753dd4d47dfc1
Oleg Samoilenko <olaggg-93so@yandex.ru>
2022-04-06 15:41:20 +0300

    d

--- script/index.js
+++ script/index.js
@@ -2,8 +2,8 @@ const profileButton = document.querySelector('.profile__edit-button');
 const pofileTitle = document.querySelector('.profile__title');
 const profileText = document.querySelector('.profile__text');
 const popupFormProfile = document.querySelector('.popup_form_profile');
-const popupItemHeading = popupFormProfile.querySelector('.popup_item_heading');
-const popupItemSubHeading = popupFormProfile.querySelector('.popup_item_subheading');
+const popupItemHeading = popupFormProfile.querySelector('.popup__item_type_heading');
+const popupItemSubHeading = popupFormProfile.querySelector('.popup__item_type_subheading');
 const popupClose = popupFormProfile.querySelector('.popup__close');
 const popupForm = popupFormProfile.querySelector('.popup__form');
 
